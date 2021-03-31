356012372b8cfdc250742a0abc9dbadb0d396373
Andy <admazzola@gmail.com>
2021-03-31 18:08:33 -0400

    just need to do sig in test

--- test/ganacheTest.js
+++ test/ganacheTest.js
@@ -5,6 +5,12 @@ import path from 'path'
 import ganache from 'ganache-cli' 
 import  Web3 from 'web3' 
 
+
+
+
+import GenerationHelper from '../lib/GenerationHelper.js' 
+import EIP712Utils from '../lib/EIP712Utils.js'
+
 const provider = ganache.provider()
 const OPTIONS = {
   defaultBlock: "latest",
@@ -12,6 +18,9 @@ const OPTIONS = {
   transactionBlockTimeout: 5
 };
 const web3 = new Web3(provider, null, OPTIONS);
+
+let customConfigJSON = fs.readFileSync(path.join('eip712-config.json'));
+let customConfig = JSON.parse(customConfigJSON)
 
 //const { abi, evm } = require('../compile');
 let contractJSON = fs.readFileSync(path.join('generated/built/MyFirstContract.json'));
@@ -30,7 +39,40 @@ describe("EIP712 Contract Testing", function() {
           .deploy({data: "0x" + evm.bytecode.object, arguments: [chainId]})
           .send({from: accounts[0], gas: 5000000});
   
-      console.log("finished")
+      let contractAddress = myEIP712Contract.options.address
+      console.log("deployed contract at ", contractAddress)
 
+
+      let dataValues = {
+        customName:"myName",
+        bidderAddress:accounts[0],
+        nftContractAddress:"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+        currencyTokenAddress:"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+        currencyTokenAmount:100,
+        requireProjectId:true,
+        projectId:123,
+        expires:50000 
+    }
+
+    const typedData = EIP712Utils.getTypedDataFromParams( 
+      chainId,  
+      contractAddress,
+      customConfig,
+      dataValues  
+    )
+    let typedDatahash = EIP712Utils.getTypedDataHash(typedData)
+
+
+
+    let signature = "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"
+
+
+
+      let args = Object.values(dataValues)
+      args.push(signature)
+
+      let result = await myEIP712Contract.methods.verifyOffchainSignatureAndDoStuff(...args).send({from: accounts[0] })
+
+      console.log("result of method call: ", result)
     });
   });
